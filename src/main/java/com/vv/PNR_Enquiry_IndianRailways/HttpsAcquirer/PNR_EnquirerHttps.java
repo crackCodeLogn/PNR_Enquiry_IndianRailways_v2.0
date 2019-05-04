32a1738aec9db6ca31a6d7a06d88c12b686b6788
crackCodeLogn <vivektheintel@gmail.com>
2019-05-04 13:28:13 +0530

    cleaning it up

--- src/main/java/com/vv/PNR_Enquiry_IndianRailways/HttpsAcquirer/PNR_EnquirerHttps.java
+++ src/main/java/com/vv/PNR_Enquiry_IndianRailways/HttpsAcquirer/PNR_EnquirerHttps.java
@@ -1,16 +1,17 @@
 package com.vv.PNR_Enquiry_IndianRailways.HttpsAcquirer;
 
-import com.vv.PNR_Enquiry_IndianRailways.GUI.PNR_Form;
-import com.vv.PNR_Enquiry_IndianRailways.LoggerFormatter;
-import com.vv.PNR_Enquiry_IndianRailways.MainActivity;
-import com.vv.PNR_Enquiry_IndianRailways.MapOfClassOfTravel;
+import com.vv.PNR_Enquiry_IndianRailways.GUI.PNR_Prompt;
+import com.vv.PNR_Enquiry_IndianRailways.GUI.PNR_Result;
 import com.vv.PNR_Enquiry_IndianRailways.Model.Passenger;
 import com.vv.PNR_Enquiry_IndianRailways.Model.PassengerList;
+import com.vv.PNR_Enquiry_IndianRailways.util.Helper;
+import com.vv.PNR_Enquiry_IndianRailways.util.MapOfClassOfTravel;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.swing.*;
-import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,7 +19,9 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.vv.PNR_Enquiry_IndianRailways.MainActivity.smallLogoPath;
+import static com.vv.PNR_Enquiry_IndianRailways.util.Constants.TITLE_PNR_BASED_TICKET_DETAILS;
+import static com.vv.PNR_Enquiry_IndianRailways.util.Constants.siteToHitForPnrStatus;
+import static com.vv.PNR_Enquiry_IndianRailways.util.Helper.showErrorMsgDialog;
 
 /**
  * This deals with the Https Acquirer for the pnr request...
@@ -30,26 +33,24 @@ import static com.vv.PNR_Enquiry_IndianRailways.MainActivity.smallLogoPath;
  * -> Major structural overhaul :
  * -- Eradicating the use of the constructor and directly linking to the function {@link #PNR_EnquirerHttpsRunner(String)}
  * -- connection linkage line commenting from line 79 to line 89
- * -- Shifting parent component of the JOptionPane in line 343 and 350 from null to independantForJOptionPane
+ * -- Shifting parent component of the JOptionPane in line 343 and 350 from null to independentForJOptionPane
  * @since 01-08-2017
  */
 public class PNR_EnquirerHttps {
 
-    /*
-    //
-    public PNR_EnquirerHttps(final String requestedPNR) {
-        try {
-            PNR_EnquirerHttpsRunner(requestedPNR);
-        } catch (IOException e) {
+    private final Logger logger = LoggerFactory.getLogger(PNR_EnquirerHttps.class);
 
-        }
+    private final MapOfClassOfTravel mapOfClassOfTravel = new MapOfClassOfTravel();
+    private final JFrame independentForJOptionPane = new JFrame();
+    private final PNR_Prompt pnr_prompt;
+
+    public PNR_EnquirerHttps(PNR_Prompt pnr_prompt) {
+        this.pnr_prompt = pnr_prompt;
     }
-    */
-    private JFrame independantForJOptionPane = new JFrame();
 
-    public static void performEnablingFromHere() {
-        MainActivity.disableAll = false;
-        MainActivity.performEnabling();
+    private void performEnablingFromHere() {
+        pnr_prompt.disableAll = false;
+        pnr_prompt.performEnabling();
     }
 
     /**
@@ -59,52 +60,32 @@ public class PNR_EnquirerHttps {
      * @throws IOException
      */
     public void PNR_EnquirerHttpsRunner(final String requestedPNR) throws IOException {
-        java.util.logging.Logger logger = java.util.logging.Logger.getLogger(PNR_EnquirerHttps.class.getName());
-        new MapOfClassOfTravel();
-        //initializing the map which stores the class and their corresponding description
-        logger = LoggerFormatter.formatTheLoggerOutput(logger);
-        //the above line should be written in each and every class where logging is to be used
 
-        int responseCode = 404;
-
-        @Nullable HttpsURLConnection httpsURLConnection = null;
-        URL url = new URL("https://www.railyatri.in/pnr-status/" + requestedPNR); //this https enabled site was allowing automated data extraction without giving any forbidden access response code
-        logger.info("URL : " + url.toString());
-
-        logger.info("Establishing connection now!");
+        int responseCode;
+        URL url = new URL(siteToHitForPnrStatus + requestedPNR); //this https enabled site was allowing automated data extraction without giving any forbidden access response code
+        logger.info("URL : {}, Establishing connection now!", url);
+        @Nullable HttpsURLConnection httpsURLConnection;
         try {
             httpsURLConnection = (HttpsURLConnection) url.openConnection();
             httpsURLConnection.connect();
 
             responseCode = httpsURLConnection.getResponseCode();
-            logger.info("Code received : " + responseCode);
+            logger.info("Code received : {}", responseCode);
             if (responseCode == HttpsURLConnection.HTTP_OK) {
                 logger.info("Connection established successfully!!");
-                /*
-            }
-        } catch (Exception e) {
-            logger.info("Connection establishment failed... \nException : " + e);
-            performEnablingFromHere();
-        }
 
-        logger.info("Establishing inflow path now, if possible...");
-        try {
-            if (responseCode == HttpsURLConnection.HTTP_OK) {
-            */
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(httpsURLConnection.getInputStream()));
-                int lineNo = 1, numberOfPassengers = 0, baseLineMark = 0;
+                int lineNo = 1, numberOfPassengers = 0;
                 String trainNumber = "", trainName = "", boardingStation = "", destinationStation = "", boardingDate = "", classOfTravel = "", chartStatus = "";
                 String line;
-                //boolean passengerDetailsReached = false;
-                //StringBuilder passengerDataToBeProcessed = new StringBuilder();
-                ArrayList<String> passengerDataToBeProcessed = new ArrayList<String>();
+                ArrayList<String> passengerDataToBeProcessed = new ArrayList<>();
                 boolean firstShot = false;
 
                 while ((line = bufferedReader.readLine()) != null) {
 
                     if (lineNo >= 480) {
                         String elemental = "";
-                        logger.info(lineNo + " --> " + line);
+                        logger.info("{} --> {}", lineNo, line);
 
                         try {
                             //elemental = line.substring(line.indexOf('>'), line.lastIndexOf('<')).trim();
@@ -127,7 +108,7 @@ public class PNR_EnquirerHttps {
                             //System.out.println("Elemented extraction : "+elemental);
 
                         } catch (Exception e2) {
-                            //System.out.println("\t\tException e2 occuring in string conversion, e2 : " + e2);
+                            //System.out.println("\t\tException e2 occuring in string conversion, e2 : ", e2);
                             //commented out, as this would occur spam the output stream
                         }
 
@@ -135,7 +116,7 @@ public class PNR_EnquirerHttps {
                         //restricting the upper limit on the line reading to 620, as there can be only max 4 passengers on a single ticket, and the last entry should end at 612
                         if (lineNo >= 620 || line.contains("pnr-result-link col-sm-12")) break;
 
-                        //if (line.contains("PNR - " + requestedPNR)) {
+                        //if (line.contains("PNR - ", requestedPNR)) {
                         if (line.contains("pnr-bold-txt") && !firstShot) {
                             //baseLineMark = lineNo + 3;
                             //baseLineMark = lineNo;
@@ -171,7 +152,7 @@ public class PNR_EnquirerHttps {
                         /*
                         //this was for the older version of the site, when this repo was last updated 7 months ago
                         if (lineNo == baseLineMark) {
-                            System.out.println("PNR first step crossed at line number : " + lineNo + ", and the base mark : " + baseLineMark);
+                            System.out.println("PNR first step crossed at line number : ", lineNo + ", and the base mark : ", baseLineMark);
                             trainNumber = line.substring(0, line.indexOf('-')).trim();
                             trainName = line.substring(line.indexOf('-') + 1).trim();
                         }
@@ -195,13 +176,13 @@ public class PNR_EnquirerHttps {
                 /*
                 //older code
                 while ((line = bufferedReader.readLine()) != null) {
-                    logger.info("Line number " + lineNo + " --> " + line);
+                    logger.info("Line number ", lineNo + " --> ", line);
 
                     //if (lineNo >= 606) {
                     //if (lineNo >= 640) {
                     if (lineNo >= 600) {
 
-                        logger.info("Line number " + lineNo + " --> " + line);
+                        logger.info("Line number ", lineNo + " --> ", line);
                         //if (lineNo >= 670 && line.contains("</table>")) break;
                         //if (lineNo >= 702 && line.contains("</table>")) break;
                         if (lineNo >= 700 && line.contains("</table>")) break;
@@ -211,11 +192,11 @@ public class PNR_EnquirerHttps {
                         //starting the data extraction from this line onwards
                         //if (lineNo == 609) {
                         //if (lineNo == 643) {
-                        if (line.contains("PNR - " + requestedPNR)) {
+                        if (line.contains("PNR - ", requestedPNR)) {
                             baseLineMark = lineNo + 3;
                         }
                         if (lineNo == baseLineMark) {
-                            System.out.println("PNR first step crossed at line number : " + lineNo + ", and the base mark : " + baseLineMark);
+                            System.out.println("PNR first step crossed at line number : ", lineNo + ", and the base mark : ", baseLineMark);
                             trainNumber = line.substring(0, line.indexOf('-')).trim();
                             trainName = line.substring(line.indexOf('-') + 1).trim();
                         }
@@ -249,38 +230,24 @@ public class PNR_EnquirerHttps {
                 httpsURLConnection.disconnect();
 
                 logger.info("The extracted details:-");
-                logger.info("Train number : " + trainNumber);
-                logger.info("Train name : " + trainName);
-                logger.info("Boarding station : " + boardingStation);
-                logger.info("Destination station : " + destinationStation);
-                logger.info("Boarding date : " + boardingDate);
-                logger.info("Class : " + classOfTravel);
-                logger.info("Chart status : " + chartStatus);
+                logger.info("Train number : {}", trainNumber);
+                logger.info("Train name : {}", trainName);
+                logger.info("Boarding station : {}", boardingStation);
+                logger.info("Destination station : {}", destinationStation);
+                logger.info("Boarding date : {}", boardingDate);
+                logger.info("Class : {}", classOfTravel);
+                logger.info("Chart status : {}", chartStatus);
                 //logger.info("Data to be processed :- \n"+passengerDataToBeProcessed);
-                logger.info("The number of passengers in this ticket : " + numberOfPassengers);
+                logger.info("The number of passengers in this ticket : {}", numberOfPassengers);
 
                 //starting the passenger data processing if everything is working properly
-                List<Passenger> listOfPassengers = new ArrayList<Passenger>();
-                //performEnablingFromHere();
+                List<Passenger> listOfPassengers = new ArrayList<>();
                 if (numberOfPassengers != 0) {
-                    /*
-                    //older code
                     int localPointer = 0;
-                    for (; localPointer < passengerDataToBeProcessed.size(); localPointer += 22) {
-                        //logger.info(passengerDataToBeProcessed.get(localPointer));
-                        Passenger passenger = new Passenger();
-                        passenger.setNumber(Integer.parseInt(passengerDataToBeProcessed.get(localPointer)));
-                        passenger.setBookingStatus(passengerDataToBeProcessed.get(localPointer + 3).trim());
-                        passenger.setCurrentStatus(passengerDataToBeProcessed.get(localPointer + 6).trim());
-                        listOfPassengers.add(passenger);
-                    }
-                    */
-                    int localPointer = 0;
-                    //for (; localPointer < passengerDataToBeProcessed.size(); localPointer += 22) {
                     int tr = 0;
                     boolean shifter = true;
                     Passenger holder = null;
-                    String elemental = "";
+                    String elemental;
                     //Extracting the details of the passengers on the ticket
                     for (; localPointer < passengerDataToBeProcessed.size(); localPointer++) {
                         //for this version of the website, shifting the extraction on the basis of the string
@@ -295,17 +262,14 @@ public class PNR_EnquirerHttps {
                                 passenger.setBookingStatus(elemental);
                                 shifter = false;
                             } else {
-                                if (holder != null) {
-                                    holder.setCurrentStatus(elemental);
-                                    listOfPassengers.add(holder);
-                                }
+                                holder.setCurrentStatus(elemental);
+                                listOfPassengers.add(holder);
                                 shifter = true;
                             }
                         }
                     }
-                    for (Passenger passenger : listOfPassengers) {
-                        logger.info(passenger.getNumber() + " :: " + passenger.getBookingStatus() + " :: " + passenger.getCurrentStatus());
-                    }
+                    listOfPassengers.forEach(passenger -> logger.info(passenger.toString()));
+
                     final PassengerList passengerList = new PassengerList();
                     passengerList.setListOfPassengers(listOfPassengers);
                     //call the function for the pnr form from here
@@ -317,49 +281,33 @@ public class PNR_EnquirerHttps {
                     final String finalDestinationStation = destinationStation;
                     final String finalBoardingStation = boardingStation;
 
-                    final java.util.logging.Logger finalLogger = logger;
-                    SwingUtilities.invokeLater(new Runnable() {
-                        @Override
-                        public void run() {
+                    SwingUtilities.invokeLater(() -> {
 
-                            JFrame frame = new JFrame("PNR based ticket details");
-                            frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                            //frame.setLocationRelativeTo(null);
-                            frame.getContentPane().add(new PNR_Form(requestedPNR, finalTrainNumber, finalTrainName, finalBoardingStation, finalDestinationStation, finalBoardingDate, finalClassOfTravel, finalChartStatus, passengerList).getUI());
+                        JFrame frame = new JFrame(TITLE_PNR_BASED_TICKET_DETAILS);
+                        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                        //frame.setLocationRelativeTo(null);
+                        frame.getContentPane().add(new PNR_Result(requestedPNR, finalTrainNumber, finalTrainName, finalBoardingStation, finalDestinationStation, finalBoardingDate, finalClassOfTravel, finalChartStatus, passengerList, mapOfClassOfTravel).getUI());
 
-                            // Create and set up the content pane.
-                            // Display the window.
-                            frame.pack();
-                            try {
-                                frame.setIconImage(new ImageIcon(Toolkit.getDefaultToolkit().createImage(MainActivity.class.getResource(smallLogoPath))).getImage());
-                            } catch (NullPointerException npe1) {
-                                finalLogger.info("ERROR in loading the image for the frame... NUll pointer exception occurred!");
-                            }
-                            frame.setResizable(false);
-                            frame.setVisible(true);
-                        }
+                        // Create and set up the content pane.
+                        // Display the window.
+                        frame.pack();
+                        frame.setIconImage(Helper.smallIconForFrame);
+                        frame.setResizable(false);
+                        frame.setVisible(true);
                     });
                 } else {
-                    logger.info("THIS PART SHALL NOT BE REACHED EVER");
-                    //logger.info("No passengers, so no processing of their data!");
-                    Toolkit.getDefaultToolkit().beep();
-                    //JOptionPane.showMessageDialog(null, "PNR record doesn't exist!", "Error", JOptionPane.ERROR_MESSAGE);
-                    JOptionPane.showMessageDialog(independantForJOptionPane, "PNR record doesn't exist!", "Error", JOptionPane.ERROR_MESSAGE);
+                    logger.warn("THIS PART SHALL NOT BE REACHED EVER");
+                    showErrorMsgDialog(independentForJOptionPane, "PNR record doesn't exist!");
                 }
             } else {
-                logger.info("No reading of webpage as connection not ok!");
-                logger.info("Connection response code : " + responseCode);
-                Toolkit.getDefaultToolkit().beep();
-                //JOptionPane.showMessageDialog(null, "No Internet Connectivity or the server is down", "Error", JOptionPane.ERROR_MESSAGE);
-                JOptionPane.showMessageDialog(independantForJOptionPane, "No Internet Connectivity or the server is down", "Error", JOptionPane.ERROR_MESSAGE);
+                logger.error("No reading of webpage as connection not ok! (Conn resp code : {})", responseCode);
+                showErrorMsgDialog(independentForJOptionPane, "No Internet Connectivity / The server is down");
             }
             httpsURLConnection.disconnect();
-            //loopON = false;
             performEnablingFromHere();
         } catch (Exception e) {
-            logger.info("Exception in the reading part... \n\tException : " + e);
+            logger.info("Exception in the reading part... ", e);
             performEnablingFromHere();
-            //loopON = false;
         }
     }
 }
