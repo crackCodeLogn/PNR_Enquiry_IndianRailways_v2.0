26c0c4a639433ceea1279d2e25203766332d7096
Vivek <vivektheintel@gmail.com>
2021-12-05 00:06:27 +0530

    Adding PNR enquiry bin and updating artifact id

---
+++ bin/pnr-enquiry.sh
@@ -0,0 +1,10 @@
+APP_NAME="enquiry-pnr"
+APP_VERSION="2.0-SNAPSHOT"
+JAVA_PARAM="-Xmx101m"
+APP_PARAMS=""
+
+BIN_PATH=$TWM_HOME_PARENT/PNR_Enquiry_IndianRailways_v2.0/bin     #PROM-HOME-PARENT :: exported in .bashrc
+JAR_PATH=$BIN_PATH/../target/$APP_NAME-$APP_VERSION.jar
+
+echo "Starting '$APP_NAME' with java param: '$JAVA_PARAM', app params: '$APP_PARAMS' at '$JAR_PATH'"
+java $JAVA_PARAM $APP_PARAMS -jar $JAR_PATH
